72a43536060c4147cb39d63217c6f3ccfbba03ec
thomashessel <thomas.silva482@gmail.com>
2023-05-04 20:32:33 -0300

    criação projeto vazio

---
+++ src/main/java/com/hessel/treinoDevSup/IntensivoJava/IntensivoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.hessel.treinoDevSup.IntensivoJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensivoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensivoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hessel/treinoDevSup/IntensivoJava/IntensivoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hessel.treinoDevSup.IntensivoJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensivoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
